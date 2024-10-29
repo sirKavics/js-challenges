fadc76226c05c75188a98405a52c926550b85d5f
sirkavics <falf555@gmail.com>
2024-10-29 00:35:28 -0400

    js beginner challenges first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Q1. Return the Sum of Two Numbers
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addition(3, 5));
+
+
+// Q2. Convert Hours into Seconds
+
+function hoursIntoSeconds(hour) {
+    return seconds = hour * 60 * 60;
+}
+
+console.log(hoursIntoSeconds(2));
+
+
+// Q3. Calculate the Perimeter of a Rectangle
+
+function calcPerimeter(length, width) {
+    return 2 * length + 2 * width;
+}
+
+console.log(calcPerimeter(10, 20));
+
+// Q4. Calculate the Area of a Triangle
+
+function calcArea(base, height) {
+    
+}
+
+console.log(calcArea(3, 2))
