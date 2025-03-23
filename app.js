c3132dc38f2569bb36b509172f17d53f73feabd9
sirkavics <falf555@gmail.com>
2025-03-22 23:52:44 -0400

    JS challenges study review

--- app.js
+++ app.js
@@ -114,3 +114,53 @@ function isLoggedInAndSubscribed(loggedIn, subscribed) {
 console.log(isLoggedInAndSubscribed("LOGGED_OUT", "UNSUBSCRIBED"));
 
 
+////////////////////// MEDIUM CHALLENGES //////////////////////
+
+
+//  Q1. Falsy or truthy?  //
+
+function filterOutFalsy(elem1, elem2) {
+  return !elem1 ? elem1 : elem2;
+}
+
+console.log(filterOutFalsy(true, "Dog"));
+
+//  Q2. Return the length of any given array  //
+
+function arrLength(arr) {
+  return arr.length;
+}
+console.log(arrLength([0, 1, 2, 3, 4, 5]));
+
+//  Q3. Get the last element in an array  //
+
+function lastElem(arr) {
+  return arr[arr.length - 1];
+}
+
+console.log(lastElem(["dog", "cat", "ball", 5, -100]));
+
+//  Q4. Find the sum of an array  //
+
+function arrSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+
+console.log(arrSum([100, 200, 500]));
+
+//  Q5. Add up the numbers from a single number  //
+
+function progressiveSum(num) {
+  let sum = 0;
+  for (let i = 0; i <= num; ++i) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(progressiveSum(3));
+
