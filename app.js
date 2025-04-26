7166d7dee1f3444e571b5486a7abf4c3ae4a2ff7
sirkavics <falf555@gmail.com>
2025-04-25 23:45:22 -0400

    ja challenges practice study

--- app.js
+++ app.js
@@ -154,3 +154,47 @@ console.log(isLoggedInOrSubscribed("LOGGED_OUT", "UNSUBSCRIBED"));
 //////////////////////// MEDIUM CHALLENGES ////////////////////////
 console.log("////// MEDIUM CHALLENGES //////");
 
+//  Q1. Falsy or truthy?  //
+//  Given two values, return the first one if it is falsy, otherwise return the 
+//  second one.
+
+function filterOutFalsy(elem1, elem2) {
+  return !elem1 ? elem1 : elem2;
+}
+console.log(filterOutFalsy(0, 500));
+console.log(filterOutFalsy(false, 100));
+
+//  Q2. Return the length of any given array  //
+//  Given an array, return it's length.
+
+function arrLength(arr) {
+  return arr.length;
+}
+console.log(arrLength([1, 2, 3]));
+console.log(arrLength([5, 0, -4, 1]));
+console.log(arrLength([]));
+
+//  Q3. Get the last element in an array  //
+//  Given an array, return the last element.
+
+function lastElem(arr) {
+  return arr[arr.length - 1];
+}
+console.log(lastElem([3, 2, 0, 6, 2]));
+console.log(lastElem(["dog", "cat", "ball"]));
+console.log(lastElem([null, 5, false]));
+
+//  Q4. Find the sum of an array  //
+//  Given an array, return the sum of every element.
+
+function arrSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(arrSum([2, 2, 2]));
+console.log(arrSum([100, 200, 500]));
+console.log(arrSum([0, -5, -10]));
+
